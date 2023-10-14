d8f5c5eb28e805152c214b4058cf577439567de4
Vladislav Vasilev <akiro_jpg@vk.com>
2023-10-14 04:05:27 +0300

    edited str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def string_to_uppercase(string: str) -> str:
+    """Возвращает строку в верхнем регистре"""
     return string.upper()
+
+
+def string_capitalize(string: str) -> str:
+    """Возвращает строку, в которой каждое слово с заглавной буквы"""
+    return string.capitalize()
